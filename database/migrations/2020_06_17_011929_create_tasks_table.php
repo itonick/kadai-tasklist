b7c5f3aa6d528187112332b602d3ecab28b066be
ito <yorizo.ito@gmail.com>
2020-06-17 07:26:07 +0000

    fix migrate

--- database/migrations/2020_06_17_011929_create_tasks_table.php
+++ database/migrations/2020_06_17_011929_create_tasks_table.php
@@ -20,7 +20,7 @@ class CreateTasksTable extends Migration
             $table->string('content');
             $table->timestamps();
             
-            $table->foreign('user_id')->references('id')->on('tasks');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
